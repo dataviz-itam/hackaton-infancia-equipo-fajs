36ef289fb01381e345619c73b9d6abdeec95753e
joseluistl <joseluis.trujillo.lopez@hotmail.com>
2024-04-28 21:53:47 -0600

    Mapa ya funciona!

--- main2.js
+++ main2.js
@@ -10,8 +10,6 @@ document.addEventListener('DOMContentLoaded', async function() {
     try {
         var dataByMun = await cargarDatos();
 
-        console.log(dataByMun);
-        
         dataByMun.forEach(function(mun) {
             var marker = L.marker(mun.coord).addTo(mymap);
             marker.bindPopup(`<b>Nombre:</b> ${mun.municipio}<br><b>Conteo de desapariciones:</b> ${mun.conteo}`);
@@ -20,4 +18,25 @@ document.addEventListener('DOMContentLoaded', async function() {
         console.error('Hubo un error al cargar los datos:', error);
     }
 });
+
+document.addEventListener('DOMContentLoaded', async function() { 
+    var mymap = L.map('mapid2').setView([16.75, -92.5], 7);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+    }).addTo(mymap);
+
+    try {
+        var dataByMun = await cargarDatos();
+
+        dataByMun.forEach(function(mun) {
+            var marker = L.marker(mun.coord).addTo(mymap);
+            marker.bindPopup(`<b>Nombre:</b> ${mun.municipio}<br><b>Conteo de desapariciones:</b> ${mun.conteo}`);
+        });
+    } catch (error) {
+        console.error('Hubo un error al cargar los datos:', error);
+    }
+});
+
+
 
